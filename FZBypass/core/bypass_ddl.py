5c95f2c217b75b72067117858cc547aea101b0ec
Pea Masamba <129601224+Yyg-Masamba@users.noreply.github.com>
2024-01-26 15:49:23 +0700

    Update bypass_ddl.py

--- FZBypass/core/bypass_ddl.py
+++ FZBypass/core/bypass_ddl.py
@@ -176,7 +176,7 @@ async def gyanilinks(url: str) -> str:
     try: 
         inputs = soup.find(id="go-link").find_all(name="input")
     except: 
-        raise DDLException("\nGagal membypass link") 
+        raise DDLException("\nGagal membypass link😊") 
     await asleep(5)
     resp = cget("POST", f"{DOMAIN}/links/go", data= { input.get('name'): input.get('value') for input in inputs }, headers={ "x-requested-with": "XMLHttpRequest" })
     try: 
